0dc0a95c000129a900d8c9b7bc309662839af69f
Zakaria Moussammi <zakariamoussammi87@gmail.com>
2023-08-08 22:49:03 +0100

    Tue Aug  8 22:49:03 +01 2023

--- inc/Request.hpp
+++ inc/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: zmoussam <zmoussam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 21:29:36 by zmoussam          #+#    #+#             */
-/*   Updated: 2023/08/08 00:58:35 by zmoussam         ###   ########.fr       */
+/*   Updated: 2023/08/08 17:20:31 by zmoussam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 #define REQUEST__
 #include <iostream>
 #include <string>
+#include <map>
 #define BUFFER_SIZE 3000
     class Request
     {
         private:
             int _clientSocket;
-            char _buffer[BUFFER_SIZE + 1];
+            std::string _buffer;
             size_t bufferLength;
             std::string _httpVersion;
             std::string _body;
-            std::string _header;
+            std::map<std::string, std::string> _header;
             std::string _URL;
             std::string _method;
+            int _keepAlive;
         public:
             Request(int client_socket);
             ~Request();
--- src/request/Request.cpp
+++ src/request/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: zmoussam <zmoussam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 21:46:08 by zmoussam          #+#    #+#             */
-/*   Updated: 2023/08/08 00:58:13 by zmoussam         ###   ########.fr       */
+/*   Updated: 2023/08/08 22:34:32 by zmoussam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@
 Request::Request(int clientSocket)
 {
     int r_bytes;
+    char tmpBuffer[BUFFER_SIZE + 1];
     this->_clientSocket = clientSocket;
-    r_bytes = recv(clientSocket, this->_buffer, BUFFER_SIZE, 0);
+    r_bytes = recv(clientSocket, tmpBuffer, BUFFER_SIZE, 0);
     if (r_bytes < 0)
         std::cout << "Error : received data failed" << std::endl;
     else
-        _buffer[r_bytes + 1] = '\0';
-    this->bufferLength = std::string(this->_buffer).size();
+        tmpBuffer[r_bytes + 1] = '\0';
+    _buffer = std::string(tmpBuffer);
+    // std::cout << _buffer << std::endl;
+    bufferLength = _buffer.size();
     
 }
 void Request::parsseRequest()
 {
     size_t nextPos = 0;
     getMethod(nextPos);
-    std::cout << "next : " << nextPos << std::endl;
+    // std::cout << "next : " << nextPos << std::endl;
     getURL(nextPos);
     getHTTPversion(nextPos);
     getHeader(nextPos);
@@ -38,34 +41,55 @@ void Request::parsseRequest()
 
 void Request::getMethod(size_t &methodPos)
 {
-    this->_method = "";
-    for (; methodPos < this->bufferLength && _buffer[methodPos] != ' '; methodPos++)
-        this->_method += _buffer[methodPos];
-    // std::cout << "method : " << this->_method << std::endl;
+    _method = "";
+    for (; methodPos < bufferLength && _buffer[methodPos] != ' '; methodPos++)
+        _method += _buffer[methodPos];
+    // std::cout << "method : " << _method << std::endl;
 }
 
 void Request::getURL(size_t &URL_Pos)
 {
-    this->_URL = "";
-    for (; URL_Pos < this->bufferLength && this->_buffer[URL_Pos] == ' '; URL_Pos++);
-    for (; URL_Pos < this->bufferLength && this->_buffer[URL_Pos] != ' '; URL_Pos++)
-        this->_URL += this->_buffer[URL_Pos];
-    // std::cout << "Path : " << this->_URL << std::endl;
+    _URL = "";
+    for (; URL_Pos < bufferLength && _buffer[URL_Pos] == ' '; URL_Pos++);
+    for (; URL_Pos < bufferLength && _buffer[URL_Pos] != ' '; URL_Pos++)
+        _URL += _buffer[URL_Pos];
+    // std::cout << "Path : " << _URL << std::endl;
 }
 
 void Request::getHTTPversion(size_t &_httpversion_Pos)
 {
-    this->_httpVersion = "";
-    for (; _httpversion_Pos < this->bufferLength && this->_buffer[_httpversion_Pos] == ' '; _httpversion_Pos++);
-    for (; _httpversion_Pos < this->bufferLength && this->_buffer[_httpversion_Pos] != ' ' \
-       && this->_buffer[_httpversion_Pos] != '\r'; _httpversion_Pos++)
-        this->_httpVersion += this->_buffer[_httpversion_Pos];
-    // std::cout << "http : " << this->_httpVersion << std::endl;
+    _httpVersion = "";
+    for (; _httpversion_Pos < bufferLength && _buffer[_httpversion_Pos] == ' '; _httpversion_Pos++);
+    for (; _httpversion_Pos < bufferLength && _buffer[_httpversion_Pos] != ' ' \
+       && _buffer[_httpversion_Pos] != '\r'; _httpversion_Pos++)
+        _httpVersion += _buffer[_httpversion_Pos];
+    // std::cout << "http : " << _httpVersion << std::endl;
 }
 
-void Request::getHeader()
+void Request::getHeader(size_t &header_pos)
 {
-
+    std::string _key;
+    size_t _headerkeyPos;
+    size_t _headerValuePos;
+    size_t i = header_pos;
+    _keepAlive = 0;
+    while (i < this->bufferLength)
+    {
+        if (_buffer[i] && (_buffer[i] == ' ' || _buffer[i] == '\r' || _buffer[i] == '\n'))
+            i++;
+        else {
+            _headerkeyPos = _buffer.find(':', i);
+            if (_headerkeyPos == std::string::npos)
+                return;
+            _key = _buffer.substr(i, _headerkeyPos - i);
+            _headerValuePos = _buffer.find("\r", _headerkeyPos);
+            this->_header[_key] = _buffer.substr(_headerkeyPos + 2, _headerValuePos - _headerkeyPos - 2);
+            if (_key == "Connection" && _header[_key] == "keep-alive")
+                _keepAlive = 1;
+            // std::cout << '$' << _key << "$ : " << '$' << this->_header[_key]  << "$" << std::endl;
+            i = _headerValuePos;
+        }
+    }
 }
 
 Request::~Request()
